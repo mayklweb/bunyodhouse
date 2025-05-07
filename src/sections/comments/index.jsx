9fb3d4877622dffdca8e39fce1ed9378666161c4
mayklweb <muhammadevvv@gmail.com>
2025-05-07 17:49:51 +0500

    m

--- src/sections/comments/index.jsx
+++ src/sections/comments/index.jsx
@@ -29,51 +29,53 @@ function Comments() {
             YUQORI SIFAT VA ARZON NARXLARDAN MAMNUN BO'LGAN BIR NECHTA
             MIZOZLARIMIZ
           </p>
-          <Swiper
-            // loop={true}
-            // effect={"fade"}
-            slidesPerView={3}
-            spaceBetween={30}
-            modules={[EffectFade, Pagination]}
-            className="mySwiper hidden"
-          >
-            <SwiperSlide className="w-[400px] bg-[#e8a900]">
-              <div className="w-[400px] p-4 text-white bg-[#e8a900]">
-                <h3>Katta rahmat!</h3>
-                <p>
-                  Uy sotib olganimizdan judayam xursandmiz. Narxlar arzon
-                  vauylar sifatli.
-                </p>
-                <div>
-                  <p>— G'oybu, Alisher</p>
+          <div className="hidden">
+            <Swiper
+              // loop={true}
+              // effect={"fade"}
+              slidesPerView={3}
+              spaceBetween={30}
+              modules={[EffectFade, Pagination]}
+              className=" hidden"
+            >
+              <SwiperSlide className="w-[400px] bg-[#e8a900]">
+                <div className="w-[400px] p-4 text-white bg-[#e8a900]">
+                  <h3>Katta rahmat!</h3>
+                  <p>
+                    Uy sotib olganimizdan judayam xursandmiz. Narxlar arzon
+                    vauylar sifatli.
+                  </p>
+                  <div>
+                    <p>— G'oybu, Alisher</p>
+                  </div>
                 </div>
-              </div>
-            </SwiperSlide>
-            <SwiperSlide className="w-[400px] p-4 text-white bg-[#e8a900]">
-              <div className="w-[400px] p-4 text-white">
-                <h3 className="text-white"> Hammaga tavsiya qilaman!</h3>
-                <p>
-                  Yaqinda oilamiz uchun uy sotib oldik. Sifatiga gap yuq.
-                  "Bunyod House"ga rahmat
-                </p>
-                <div>
-                  <p>— G'oybu, Alisher</p>
+              </SwiperSlide>
+              <SwiperSlide className="w-[400px] p-4 text-white bg-[#e8a900]">
+                <div className="w-[400px] p-4 text-white">
+                  <h3 className="text-white"> Hammaga tavsiya qilaman!</h3>
+                  <p>
+                    Yaqinda oilamiz uchun uy sotib oldik. Sifatiga gap yuq.
+                    "Bunyod House"ga rahmat
+                  </p>
+                  <div>
+                    <p>— G'oybu, Alisher</p>
+                  </div>
                 </div>
-              </div>
-            </SwiperSlide>
-            <SwiperSlide className="w-[400px] p-4 text-white bg-[#e8a900]">
-              <div className="w-[400px] p-4 text-white bg-[#e8a900]">
-                <h3> Ajoyib narxlar!</h3>
-                <p>
-                  2 oy oldin uy sotib oldik. Judayam mamnunmiz "Bunyod House"
-                  dan
-                </p>
-                <div className="w-full flex align-end justify-between">
-                  <p>— Urganch, Severa</p>
+              </SwiperSlide>
+              <SwiperSlide className="w-[400px] p-4 text-white bg-[#e8a900]">
+                <div className="w-[400px] p-4 text-white bg-[#e8a900]">
+                  <h3> Ajoyib narxlar!</h3>
+                  <p>
+                    2 oy oldin uy sotib oldik. Judayam mamnunmiz "Bunyod House"
+                    dan
+                  </p>
+                  <div className="w-full flex align-end justify-between">
+                    <p>— Urganch, Severa</p>
+                  </div>
                 </div>
-              </div>
-            </SwiperSlide>
-          </Swiper>
+              </SwiperSlide>
+            </Swiper>
+          </div>
 
           <div className="flex flex-col lg:flex-row my-10 gap-10 align-top justify-between">
             <div className="w-full lg:w-1/3 p-6 text-white bg-[#e8a900]">
