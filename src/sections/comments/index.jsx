d7b5592f26710ce57c69eecef94088011bd86596
mayklweb <muhammadevvv@gmail.com>
2025-05-08 13:58:06 +0500

    mm

--- src/sections/comments/index.jsx
+++ src/sections/comments/index.jsx
@@ -29,55 +29,67 @@ function Comments() {
             YUQORI SIFAT VA ARZON NARXLARDAN MAMNUN BO'LGAN BIR NECHTA
             MIZOZLARIMIZ
           </p>
-          <div className="hidden">
+          <div className="md:hidden">
             <Swiper
               // loop={true}
               // effect={"fade"}
-              slidesPerView={3}
+              slidesPerView={1}
               spaceBetween={30}
               modules={[EffectFade, Pagination]}
-              className=" hidden"
+              className=""
             >
               <SwiperSlide className="w-[400px] bg-[#e8a900]">
-                <div className="w-[400px] p-4 text-white bg-[#e8a900]">
-                  <h3>Katta rahmat!</h3>
-                  <p>
-                    Uy sotib olganimizdan judayam xursandmiz. Narxlar arzon
-                    vauylar sifatli.
+                <div className="w-full md:w-1/3 p-4 md:p-6 text-white bg-[#e8a900]">
+                  <h3 className="text-4xl md:text-4xl lg:text-5xl font-semibold">
+                    Katta rahmat!
+                  </h3>
+                  <p className="text-lg md:text-lg lg:text-xl mt-4">
+                    Uy sotib olganimizdan judayam xursandmiz. Narxlar arzon va
+                    uylar sifatli.
                   </p>
-                  <div>
-                    <p>— G'oybu, Alisher</p>
+                  <div className="w-full mt-8 flex justify-end">
+                    <p className="text-2xl md:text-xl lg:text-2xl">
+                      — G'oybu, Alisher
+                    </p>
                   </div>
                 </div>
               </SwiperSlide>
-              <SwiperSlide className="w-[400px] p-4 text-white bg-[#e8a900]">
-                <div className="w-[400px] p-4 text-white">
-                  <h3 className="text-white"> Hammaga tavsiya qilaman!</h3>
-                  <p>
-                    Yaqinda oilamiz uchun uy sotib oldik. Sifatiga gap yuq.
-                    "Bunyod House"ga rahmat
-                  </p>
-                  <div>
-                    <p>— G'oybu, Alisher</p>
-                  </div>
-                </div>
-              </SwiperSlide>
-              <SwiperSlide className="w-[400px] p-4 text-white bg-[#e8a900]">
-                <div className="w-[400px] p-4 text-white bg-[#e8a900]">
-                  <h3> Ajoyib narxlar!</h3>
-                  <p>
+              <SwiperSlide className="w-[400px] text-white bg-[#e8a900]">
+                <div className="w-full md:w-1/3 p-4 md:p-6 text-white bg-[#e8a900]">
+                  <h3 className="text-4xl md:text-4xl lg:text-5xl font-semibold">
+                    Hammaga tavsiya qilaman!
+                  </h3>
+                  <p className="text-lg md:text-lg lg:text-xl mt-4">
                     2 oy oldin uy sotib oldik. Judayam mamnunmiz "Bunyod House"
-                    dan
+                    dan.
                   </p>
-                  <div className="w-full flex align-end justify-between">
-                    <p>— Urganch, Severa</p>
+                  <div className="w-full mt-8 flex justify-end">
+                    <p className="text-xl md:text-xl lg:text-2xl">
+                      — Urganch, Sarvar
+                    </p>
+                  </div>
+                </div>
+              </SwiperSlide>
+              <SwiperSlide className="w-[400px] text-white bg-[#e8a900]">
+                <div className="w-full md:w-1/3 p-4 md:p-6 text-white bg-[#e8a900]">
+                  <h3 className="text-4xl md:text-4xl lg:text-5xl font-semibold">
+                    Ajoyib narxlar!
+                  </h3>
+                  <p className="text-lg md:text-lg lg:text-xl mt-4">
+                    Yaqinda oilamiz uchun uy sotib oldik. Sifatiga gap yuq.
+                    "Bunyod House"ga rahmat.
+                  </p>
+                  <div className="w-full mt-8 flex justify-end">
+                    <p className="text-2xl md:text-xl lg:text-2xl">
+                      — Urganch, Severa
+                    </p>
                   </div>
                 </div>
               </SwiperSlide>
             </Swiper>
           </div>
 
-          <div className="flex flex-col md:flex-row my-10 gap-6 md:gap-8 lg:gap-10 align-top justify-between">
+          <div className="hidden md:flex flex-col md:flex-row my-10 gap-6 md:gap-8 lg:gap-10 align-top justify-between">
             <div className="w-full md:w-1/3 p-6 text-white bg-[#e8a900]">
               <h3 className="text-4xl md:text-4xl lg:text-5xl font-semibold">
                 Katta rahmat!
@@ -87,7 +99,9 @@ function Comments() {
                 sifatli.
               </p>
               <div className="w-full mt-8 flex justify-end">
-                <p className="text-2xl md:text-xl lg:text-2xl">— G'oybu, Alisher</p>
+                <p className="text-2xl md:text-xl lg:text-2xl">
+                  — G'oybu, Alisher
+                </p>
               </div>
             </div>
             <div className="w-full md:w-1/3 p-6 text-white bg-[#e8a900]">
@@ -98,7 +112,9 @@ function Comments() {
                 2 oy oldin uy sotib oldik. Judayam mamnunmiz "Bunyod House" dan.
               </p>
               <div className="w-full mt-8 flex justify-end">
-                <p className="text-xl md:text-xl lg:text-2xl">— Urganch, Sarvar</p>
+                <p className="text-xl md:text-xl lg:text-2xl">
+                  — Urganch, Sarvar
+                </p>
               </div>
             </div>
             <div className="w-full md:w-1/3 p-6 text-white bg-[#e8a900]">
@@ -110,7 +126,9 @@ function Comments() {
                 House"ga rahmat.
               </p>
               <div className="w-full mt-8 flex justify-end">
-                <p className="text-2xl md:text-xl lg:text-2xl">— Urganch, Severa</p>
+                <p className="text-2xl md:text-xl lg:text-2xl">
+                  — Urganch, Severa
+                </p>
               </div>
             </div>
           </div>
