938f714722f0ec97d1921248ad338af4d0116a6f
mayklweb <muhammadevvv@gmail.com>
2025-05-08 13:06:05 +0500

    fix

--- src/sections/comments/index.jsx
+++ src/sections/comments/index.jsx
@@ -79,10 +79,10 @@ function Comments() {
 
           <div className="flex flex-col md:flex-row my-10 gap-6 md:gap-8 lg:gap-10 align-top justify-between">
             <div className="w-full md:w-1/3 p-6 text-white bg-[#e8a900]">
-              <h3 className="text-5xl md:text-4xl lg:text-5xl font-semibold">
+              <h3 className="text-4xl md:text-4xl lg:text-5xl font-semibold">
                 Katta rahmat!
               </h3>
-              <p className="text-xl md:text-lg lg:text-xl mt-4">
+              <p className="text-lg md:text-lg lg:text-xl mt-4">
                 Uy sotib olganimizdan judayam xursandmiz. Narxlar arzon va uylar
                 sifatli.
               </p>
@@ -91,10 +91,10 @@ function Comments() {
               </div>
             </div>
             <div className="w-full md:w-1/3 p-6 text-white bg-[#e8a900]">
-              <h3 className="text-5xl md:text-4xl lg:text-5xl font-semibold">
+              <h3 className="text-4xl md:text-4xl lg:text-5xl font-semibold">
                 Hammaga tavsiya qilaman!
               </h3>
-              <p className="text-xl md:text-lg lg:text-xl mt-4">
+              <p className="text-lg md:text-lg lg:text-xl mt-4">
                 2 oy oldin uy sotib oldik. Judayam mamnunmiz "Bunyod House" dan.
               </p>
               <div className="w-full mt-8 flex justify-end">
@@ -102,10 +102,10 @@ function Comments() {
               </div>
             </div>
             <div className="w-full md:w-1/3 p-6 text-white bg-[#e8a900]">
-              <h3 className="text-5xl md:text-4xl lg:text-5xl font-semibold">
+              <h3 className="text-4xl md:text-4xl lg:text-5xl font-semibold">
                 Ajoyib narxlar!
               </h3>
-              <p className="text-xl md:text-lg lg:text-xl mt-4">
+              <p className="text-lg md:text-lg lg:text-xl mt-4">
                 Yaqinda oilamiz uchun uy sotib oldik. Sifatiga gap yuq. "Bunyod
                 House"ga rahmat.
               </p>
