0f99e2d4813ad4c541296a833363f5025ca6116f
mayklweb <muhammadevvv@gmail.com>
2025-05-09 10:05:30 +0500

    about

--- src/sections/project/index.jsx
+++ src/sections/project/index.jsx
@@ -6,88 +6,81 @@ import SplitText from "gsap/SplitText";
 gsap.registerPlugin(SplitText, ScrollTrigger);
 
 function Project() {
-  const projectImg = useRef(null);
-
-  useEffect(() => {
-    gsap.set(projectImg.current, {
-      clipPath: "polygon(0 0%, 100% 0%, 100% 0%, 0% 0%)", // collapsed at top
-    });
-    
-    const tl = gsap.timeline({
-      scrollTrigger: {
-        trigger: projectImg.current,
-        start: "top center",
-        end: "center top",
-        scrub: 1,
-        toggleActions: "play none none none",
-      },
-    });
-    
-    tl.to(projectImg.current, {
-      clipPath: "polygon(0 0%, 100% 0%, 100% 100%, 0% 100%)", // fully revealed from top to bottom
-      ease: "power4.out",
-      duration: 2.4,
-    });
-    
-
-    document.fonts.ready.then(() => {
-      let containers = gsap.utils.toArray(".con");
-
-      containers.forEach((container) => {
-        let text = container.querySelector(".split");
-        let animation;
-
-        SplitText.create(text, {
-          type: "words,lines",
-          mask: "lines",
-          linesClass: "line",
-          autoSplit: true,
-          onSplit: (instance) => {
-            return gsap.from(instance.lines, {
-              yPercent: 120,
-              stagger: 0.1,
-              scrollTrigger: {
-                trigger: container,
-                // markers: true,
-                scrub: true,
-                start: "clamp(top center)",
-                end: "clamp(bottom center)",
-              },
-            });
-          },
-        });
-      });
-    });
-  }, []);
+  const data = [
+    {
+      id: 1,
+      image: "/project.webp",
+      area: "110.34 m2",
+      location: "Urganch, G'oybu",
+      year: "2023",
+      type: "House",
+      room: 4,
+    },
+    {
+      id: 1,
+      image: "/project.webp",
+      area: "110.34 m2",
+      location: "Urganch, G'oybu",
+      year: "2023",
+      type: "House",
+      room: 4,
+    },
+    {
+      id: 1,
+      image: "/project.webp",
+      area: "110.34 m2",
+      location: "Urganch, G'oybu",
+      year: "2023",
+      type: "House",
+      room: 4,
+    },
+    {
+      id: 1,
+      image: "/project.webp",
+      area: "110.34 m2",
+      location: "Urganch, G'oybu",
+      year: "2023",
+      type: "House",
+      room: 4,
+    },
+  ];
 
   return (
     <div className="mt-16">
-      <div className="w-full px-[4%] flex flex-col md:flex-row gap-10">
-        <div className="w-full md:w-2/4 mt-4 lg:mt-14">
-          <div className="con">
-            <h1 className="split text-[#e8a900] text-4xl lg:text-7xl font-extrabold text-center">
-              BUNYOD HOUSE
-            </h1>
-          </div>
-          <div className="con">
-            <h3 className="split text-sm lg:text-lg mt-2 font-normal text-justify">
-              "Bunyod House" qurilish firmasi Xorazm viloyatida 2019-yilda
-              tashkil topgan. Faoliyatimizning birinchi kunlaridan boshlab biz
-              mijozlarimizga yuqori sifatli uylarni taqdim qilib kelmoqdamiz.
-              Bizning uylar o'zining yuqori did va sifat bilan qurilganligi
-              sababli boshqalardan ajralib turadi. Endilikda siz ham uy sotib
-              olishga buyurtma berishingiz mumkin. Yuqori sifat va arzon narxlar
-              faqat bizda!
-            </h3>
-          </div>
-        </div>
-        <div className="w-full md:w-2/4">
-          <img
-            ref={projectImg}
-            src="/project-home-2.webp"
-            alt=""
-            className="w-full"
-          />
+      <div className="container">
+        <div className="grid grid-row-4 md:grid-cols-4 gap-10">
+          {data.map((item, i) => (
+            <div key={i} className="w-full p-5 border border-[#e8a900] flex flex-col gap-5">
+              <div>
+                <img src={item.image} alt="" />
+              </div>
+              <div>
+                <div className="flex gap-2">
+                  <p className="text-[#1e1e1e]">Maydon</p>
+                  <span className="mb-[1%] flex flex-auto border-b border-[#e8a900] border-dashed"></span>
+                  <p>
+                    110.34 m<sup>2</sup>
+                  </p>
+                </div>
+                <div className="flex gap-2">
+                  <p className="text-[#1e1e1e]">Xonalar</p>
+                  <span className="mb-[1%] flex flex-auto border-b border-[#e8a900] border-dashed"></span>
+                  <p>5</p>
+                </div>
+
+                <div className="flex gap-2">
+                  <p className="text-[#1e1e1e]">Qavat</p>
+                  <span className="mb-[1%] flex flex-auto border-b border-[#e8a900] border-dashed"></span>
+                  <p>2</p>
+                </div>
+                <div className="flex gap-2">
+                  <p className="text-[#1e1e1e]">Location</p>
+                  <span className="mb-[1%] flex flex-auto border-b border-[#e8a900] border-dashed"></span>
+                  <p>{item.location}</p>
+                </div>
+              </div>
+            </div>
+          ))}
         </div>
       </div>
     </div>
