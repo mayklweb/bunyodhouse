5e4c4c549e15f74de9f853752a41b5c99786f1df
mayklweb <muhammadevvv@gmail.com>
2025-05-14 13:22:28 +0500

    fixing

--- src/sections/info/index.jsx
+++ src/sections/info/index.jsx
@@ -39,22 +39,22 @@ function Info() {
         <div className="container py-20 bg-[url('/house-3.svg')] bg-no-repeat bg-left-bottom bg-[length:100%_50%] lg:bg-[length:60%_100%]">
           <h1 className="text-4xl text-[#FFC045] ">Info</h1>
 
-          <div className="con flex flex-col gap-14 items-end ">
-            <div className="con w-11/12 lg:w-2/4 flex gap-4">
-              <h1 className="split text-[#FFC045] text-lg md:text-xl lg:text-4xl">
+          <div className="con flex flex-col gap-14 items-end mt-10">
+            <div className="con w-full lg:w-2/4 flex gap-2 md:gap-4">
+              <h1 className="split text-[#FFC045] text-xl md:text-xl lg:text-4xl">
                 01.
               </h1>
-              <h1 className="split text-[#030303] text-xl md:text-2xl lg:text-5xl">
+              <h1 className="split text-[#030303] text-2xl md:text-2xl lg:text-5xl text-justify">
                 "Bunyod House" uylari o'zining zamonaviy dizayni bilan ajralib
                 turadi.
               </h1>
             </div>
 
-            <div className="con w-11/12 lg:w-2/4 flex gap-4">
-              <h1 className="split text-[#FFC045] text-lg md:text-xl lg:text-4xl">
+            <div className="con w-full lg:w-2/4 flex gap-4">
+              <h1 className="split text-[#FFC045] text-xl md:text-xl lg:text-4xl">
                 02.
               </h1>
-              <h1 className="split text-[#030303] text-xl md:text-2xl lg:text-5xl">
+              <h1 className="split text-[#030303] text-2xl md:text-2xl lg:text-5xl">
                 Biz mijozlarimiz uchun eng qulay narxlarni taqdim etamiz.
               </h1>
             </div>
