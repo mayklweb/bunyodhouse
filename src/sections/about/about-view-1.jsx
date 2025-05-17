31c012fe7a3aec32cdd006338cb52880d1432bf3
mayklweb <muhammadevvv@gmail.com>
2025-05-17 10:23:50 +0500

    about

--- src/sections/about/about-view-1.jsx
+++ src/sections/about/about-view-1.jsx
@@ -13,30 +13,30 @@ function AboutView1() {
         <section className="py-10">
             <div className="container">
                 <h1 className="text-4xl text-[#FFC045]">Biz xaqimizda</h1>
-                <div className="w-full mt-10 bg-no-repeat bg-center bg-contain bg-[url('/about.webp')] flex flex-col-reverse md:flex-row gap-10 bg-[length: 100%]">
-                    <div className="w-2/4">
-                        <h1 className="text-4xl">
+                <div className="w-full mt-10 bg-no-repeat bg-center bg-contain bg-[url('/about.webp')] flex flex-col md:flex-row gap-10 bg-[length: 100%]">
+                    <div className="w-full text-justify lg:w-2/4">
+                        <h1 className="text-xl lg:text-4xl">
                             "Bunyod House" qurilish firmasi Xorazm viloyatida 2019-yilda
                             tashkil topgan. Faoliyatimizning birinchi kunlaridan boshlab biz
                             mijozlarimizga yuqori sifatli uylarni taqdim qilib kelmoqdamiz.
                         </h1>
                     </div>
-                    <div className="w-2/4 grid grid-cols-2 gap-4">
+                    <div className="w-full lg:w-2/4 grid grid-cols-2 gap-4">
                         <div className="flex flex-col items-center justify-start">
-                            <p className="text-8xl">99+</p>
-                            <p className="text-2xl">DAN ORTIQ BAXTLI OILALAR</p>
+                            <p className="text-4xl lg:text-8xl">99+</p>
+                            <p className="text-sm lg:text-2xl text-center">DAN ORTIQ BAXTLI OILALAR</p>
                         </div>
-                        <div className="flex flex-col items-center justify-center">
-                            <p>+99</p>
-                            <p>xonadon</p>
+                        <div className="flex flex-col items-center justify-start">
+                            <p className="text-4xl lg:text-8xl">99+</p>
+                            <p className="text-sm lg:text-2xl text-center">XONADON</p>
                         </div>
-                        <div className="flex flex-col items-center justify-center">
-                            <p>+99</p>
-                            <p>ta muvaffaqiyatli loyiha</p>
+                        <div className="flex flex-col items-center justify-start">
+                            <p className="text-4xl lg:text-8xl">99+</p>
+                            <p className="text-sm lg:text-2xl text-center">TA MUVAFFAQIYATLI LOYIHA</p>
                         </div>
-                        <div className="flex flex-col items-center justify-center">
-                            <p>+99</p>
-                            <p>lorem</p>
+                        <div className="flex flex-col items-center justify-start">
+                            <p className="text-4xl lg:text-8xl">99+</p>
+                            <p className="text-sm lg:text-2xl text-center">TA MUVAFFAQIYATLI LOYIHA</p>
                         </div>
                     </div>
                 </div>
